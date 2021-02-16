4d7a72be38451d2a21a6055ee55d7ccf3ba469e9
Vera de Kok <veradekok@gmail.com>
2021-02-16 16:16:50 +0100

    Update extract-data2.py
    + volgnummer column
    + Description EN/NL column
    + Label EN/NL column
    + translation language labels -> Wikidata Q-numbers

--- scripts/extract-data2.py
+++ scripts/extract-data2.py
@@ -4,11 +4,25 @@ import re
 import xmltodict
 import pandas as pd
 
-filename = "Heyblocq.xml"
+filename = "J. Heyblocq 131H26.xml"
+
+languages = {
+    'Dutch; Flemish': 'Q7411', #just Dutch
+    'French': 'Q150',
+    'Latin' : 'Q397',
+    'German': 'Q188',
+    'Hebrew': 'Q9288',
+    'English': 'Q1860',
+    'Greek, Ancient (to 1453)' :'Q35497',
+    'Italian' :'Q652'
+}
 
 outlist = {}
 with open(filename, encoding="UTF-8") as fd:
     doc = xmltodict.parse(fd.read())
+    album_name = [x['#text'] for x in doc['srw:searchRetrieveResponse']['srw:records']['srw:record'][0]['srw:recordData']['srw_dc:dc']['dcterms:isPartOf'] if '@dcx:recordIdentifier' in x][0]
+    album_name = re.sub(r'(Album amicorum van )?(.+?);.+', r'\2', album_name).strip()
+    album_code = "AA"+"".join(re.findall(r'[A-Z]', album_name))
     for i in range(len(doc['srw:searchRetrieveResponse']['srw:records']['srw:record'])):
         out = {}
         row = doc['srw:searchRetrieveResponse']['srw:records']['srw:record'][i]['srw:recordData']['srw_dc:dc']
@@ -23,15 +37,22 @@ with open(filename, encoding="UTF-8") as fd:
         out['shelfmark']  = [x['#text'] for x in row['dc:identifier'] if x['@xsi:type'] == 'shelfmark'][0]
         out['shelfmark']  = out['shelfmark'].replace('Den Haag, Koninklijke Bibliotheek : ', '')
         out['kbcatlinks'] = out['kbcatlinks'].replace("http://resolver.kb.nl/resolve?urn=PPN:", "")
+        out['kbcatahidden'] = "https://opc-kb.oclc.org/DB=1/SET=1/TTL=1/PRS=PP/PPN?PPN="+out['kbcatlinks']
+        out['kbcatlinks']   = "https://opc-kb.oclc.org/DB=1/PPN?PPN="+out['kbcatlinks']
         if 'dc:language' in row:
-            out['lang'] = [row['dc:language'][x]['#text'] for x in range(len(row['dc:language'])) if '@xml:lang' in row['dc:language'][x] 
-                                                                           and row['dc:language'][x]['@xml:lang'] == 'en']
+            out['lang'] = []
+            for x in range(len(row['dc:language'])): 
+                if '@xml:lang' in row['dc:language'][x] and row['dc:language'][x]['@xml:lang'] == 'en':
+                    lang = row['dc:language'][x]['#text']
+                    if lang in languages:
+                        out['lang'].append(languages.get(lang))
+                    else:
+                        out['lang'].append(lang)
         if 'dcx:annotation' in row: 
-            if isinstance(row['dcx:annotation'], list):
-                out['annotations']  = [x          for x in row['dcx:annotation'] if isinstance(x, str)] 
-                out['annotations'] += [x['#text'] for x in row['dcx:annotation'] if isinstance(x, dict)]
-            else:
-                out['annotations']  = [row['dcx:annotation']["#text"]] if isinstance(row['dcx:annotation'], dict) else [row['dcx:annotation']]
+            if not isinstance(row['dcx:annotation'], list):
+                row['dcx:annotation'] = [row['dcx:annotation']]
+            out['annotations']  = [x          for x in row['dcx:annotation'] if isinstance(x, str)] 
+            out['annotations'] += [x['#text'] for x in row['dcx:annotation'] if isinstance(x, dict)]
             out['annotations'].reverse()
         out['creator'] = []
         if 'dc:creator' not in row:
@@ -46,12 +67,12 @@ with open(filename, encoding="UTF-8") as fd:
                 label = dccreator if isinstance(dccreator, str) else dccreator['#text']
                 creator['name'] = re.sub(r'^([^\(,]+),?([^\(]*).*', r'\2 \1', label).replace('  ', ' ').strip()
                 creator['isni'] = re.sub(r'.+\(ISNI (.+?)\)', r'\1', label) if re.match(r'.+\(ISNI .+?\)', label) else ""
-                if '@dcx:recordIdentifier' in dccreator:
-                    creator['nta']     = dccreator['@dcx:recordIdentifier'][3:] 
-                    creator['nta-url'] = 'http://data.bibliotheken.nl/id/thes/p' + creator['nta']
                 if re.match(r'.+?\(.*-.*\).*', label):
                     creator['birthyear'] = re.sub(r'.+?\((.*?)-.+', r'\1', label)
                     creator['deadyear']  = re.sub(r'.+?\(.*?-(.*?)\).*', r'\1', label)
+                if '@dcx:recordIdentifier' in dccreator:
+                    creator['nta']     = dccreator['@dcx:recordIdentifier'][3:] 
+                    creator['nta-url'] = 'http://data.bibliotheken.nl/id/thes/p' + creator['nta']
                 out['creator'].append(creator)
         outlist[out['pagelist'][0]] = out
 #output to excel:
@@ -63,21 +84,27 @@ maxLenghts["images"]      = max([len(outlist[page]["images"])      for page in o
 maxLenghts["lang"]        = max([len(outlist[page]["lang"])        for page in outlist if "lang"        in outlist[page]])
 maxLenghts["annotations"] = max([len(outlist[page]["annotations"]) for page in outlist if "annotations" in outlist[page]])
 
-dfcolumns = ['page', 'title', 'date', 'PPN kbcatlinks', 'oclc', 'shelfmark']
+dfcolumns = ['volgnr', 'album_name', 'album_code','page', 'title', 'date', 'PPN kbcatlinks','PPN kbcatahidden',  'oclc', 'shelfmark']
 
 for x in maxLenghts:
     if x == "creator":
         for y in range(maxLenghts[x]):
-            dfcolumns += ['name'+str(y+1), 'birthyear'+str(y+1), 'deadyear'+str(y+1), 'nta'+str(y+1), 'nta-url'+str(y+1), 'isni'+str(y+1)]
+            dfcolumns += ['Label EN'+str(y+1), 'Label NL'+str(y+1), 'Des EN'+str(y+1), 'Des NL'+str(y+1), 'name'+str(y+1), 'birthyear'+str(y+1), 'deadyear'+str(y+1), 'nta'+str(y+1), 'nta-url'+str(y+1), 'isni'+str(y+1)]
     else:
         dfcolumns += [x+str(y+1) for y in range(maxLenghts[x])]
 df = pd.DataFrame(columns=dfcolumns)
+index = 1;
 for page in sorted(outlist):
     row = outlist[page]
-    contribtuple = [page, 
+    volgnr = str(index).zfill(3)
+    contribtuple = [volgnr,
+        album_name,
+        album_code,
+        page,
         row.get('title'),
         row.get('date'),
         row.get('kbcatlinks'),
+        row.get('kbcatahidden'),
         row.get('OCLC'),
         row.get('shelfmark')]
     for x in maxLenghts:
@@ -85,6 +112,10 @@ for page in sorted(outlist):
             if x == 'creator':
                 try:
                     contribtuple += [
+                    album_code+"-"+volgnr+' Contribution of '+row[x][y].get('name'), 
+                    album_code+"-"+volgnr+' Bijdrage van ' +  row[x][y].get('name'), 
+                    'contribution of '+row[x][y].get('name')+' to album amicorum of '+album_name, 
+                    'albumbijdrage van ' +  row[x][y].get('name')+' aan album amicorum van '+album_name, 
                     row[x][y].get('name'),
                     row[x][y].get('birthyear'),
                     row[x][y].get('deadyear'),
@@ -92,7 +123,7 @@ for page in sorted(outlist):
                     row[x][y].get('nta-url'),
                     row[x][y].get('isni')]
                 except IndexError as e:
-                    contribtuple += ["","","","","",""]
+                    contribtuple += ["","","","","","","","","",""]
             else:
                 try:
                     contribtuple.append(row[x][y])
@@ -100,5 +131,6 @@ for page in sorted(outlist):
                     contribtuple.append("")
     s = pd.Series(contribtuple, index=df.columns)
     df = df.append(s, ignore_index=True)
+    index += 1
 print(df)
 df.to_excel("contributors-"+filename[:-4]+".xlsx")
